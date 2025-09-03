dc10a7fdcd5e6656ac77f059334d217560522a09
SharpenedMindTechSolutions <sharpenmind822@gmail.com>
2025-09-03 16:26:17 +0530

    firstcommit

---
+++ Routes/userRoutes.js
@@ -0,0 +1,49 @@
+import express from "express";
+import {
+  createaddBusiness,
+  getUserBusinesses,
+  getUserProfile,
+  updateUserProfile,
+  getAds,
+  deleteBusiness,
+  updateBusiness,
+  searchBusinesses,
+  getAllBusiness,
+  getBusinessesByCategory,
+  getBusinessId,
+} from "../Controllers/usercontroller.js";
+import { protect } from "../Middleware/authmiddleware.js";
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, path.join(process.cwd(), "uploads")),
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+const router = express.Router();
+
+/* USER ROUTES */
+router.post(
+  "/create-business",
+  protect,
+  upload.single("image"),
+  createaddBusiness
+);
+router.get("/businesses", protect, getUserBusinesses);
+router.get("/profile", protect, getUserProfile);
+router.put("/profile", protect, updateUserProfile);
+router.put("/business/:id", protect, updateBusiness);
+router.delete("/business/:id", protect, deleteBusiness);
+router.get("/getads", getAds);
+
+router.get("/search", searchBusinesses);
+router.get("/getall-business", getAllBusiness);
+router.get("/getall-category", getBusinessesByCategory);
+router.get("/get-categorybusiness/:id", getBusinessId);
+
+export default router;
---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./DB/connectDB.js";
+import authRoutes from "./Routes/authRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
+import adminRoutes from "./Routes/adminRoutes.js";
+import adRoutes from "./Routes/adRoutes.js";
+
+dotenv.config();
+connectDB();
+const app = express();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+   "http://localhost:5174",
+  "https://www.myproductiondomain.com",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
+app.use("/api/auth/", authRoutes);
+app.use("/api/user/ads/", userRoutes);
+app.use("/api/admin/", adminRoutes);
+app.use("/api/admin/ads/", adRoutes);
+app.use("/uploads", express.static("uploads"));
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
