f85a3d8526bdadbf12909d8cb44f7d41f8cb6d16
SharpenedMindTechSolutions <sharpenmind822@gmail.com>
2025-09-03 17:05:50 +0530

    third commit

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-   "http://localhost:5174",
-  "https://sterlingyellowpaage.netlify.app/",
+  "https://sterlingyellowpaage.netlify.app/"
 ];
 
 const corsOptions = {
